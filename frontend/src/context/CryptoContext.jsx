4b5183772cc1c9af57f667d3edb97919e1750592
thilak2607 <r.thilageshwar2607@gmail.com>
2026-03-31 11:06:09 +0530

    Delete frontend directory

--- frontend/src/context/CryptoContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export const CryptoContext = createContext({
-  cryptoData: {},
-  portfolio: null,
-  refreshPortfolio: async () => {},
-  socket: null,
-  userId: 'mock-trader-01'
-});
